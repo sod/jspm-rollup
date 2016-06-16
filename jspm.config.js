22ef20b3f7b67f7a11517bf67ac166d4680316bf
Alexander von Weiss <alexvonweiss@googlemail.com>
2016-06-16 23:22:49 +0200

    jspm init

---
+++ jspm.config.js
@@ -0,0 +1,50 @@
+SystemJS.config({
+  paths: {
+    "github:": "jspm_packages/github/",
+    "npm:": "jspm_packages/npm/",
+    "jspm-17-rollup/": "src/"
+  },
+  browserConfig: {
+    "baseURL": "/"
+  },
+  devConfig: {
+    "map": {
+      "plugin-typescript": "github:frankwallis/plugin-typescript@4.0.16",
+      "os": "github:jspm/nodelibs-os@0.2.0-alpha"
+    },
+    "packages": {
+      "github:frankwallis/plugin-typescript@4.0.16": {
+        "map": {
+          "typescript": "npm:typescript@1.8.10"
+        }
+      },
+      "github:jspm/nodelibs-os@0.2.0-alpha": {
+        "map": {
+          "os-browserify": "npm:os-browserify@0.2.1"
+        }
+      }
+    }
+  },
+  transpiler: "plugin-typescript",
+  packages: {
+    "jspm-17-rollup": {
+      "main": "index.ts",
+      "format": "esm",
+      "meta": {
+        "*.ts": {
+          "loader": "plugin-typescript"
+        }
+      }
+    }
+  }
+});
+
+SystemJS.config({
+  packageConfigPaths: [
+    "github:*/*.json",
+    "npm:@*/*.json",
+    "npm:*.json"
+  ],
+  map: {},
+  packages: {}
+});
