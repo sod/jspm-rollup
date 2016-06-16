ca2ff0dfc89f2aac7ed4c0074128fc5fa1f9bfcb
Alexander von Weiss <alexvonweiss@googlemail.com>
2016-06-17 01:27:53 +0200

    working angular and rxjs rollup build

--- jspm.config.js
+++ jspm.config.js
@@ -1,4 +1,7 @@
 SystemJS.config({
+  typescriptOptions: {
+    "target": "es2015"
+  },
   paths: {
     "github:": "jspm_packages/github/",
     "npm:": "jspm_packages/npm/",
@@ -46,6 +49,14 @@ SystemJS.config({
     "npm:@*/*.json",
     "npm:*.json"
   ],
-  map: {},
+  map: {
+    "@angular/common": "npm:@angular/common@2.0.0-rc.2",
+    "@angular/core": "npm:@angular/core@2.0.0-rc.2",
+    "process": "github:jspm/nodelibs-process@0.2.0-alpha",
+    "reflect-metadata": "npm:reflect-metadata@0.1.3",
+    "rxjs": "npm:rxjs-es@5.0.0-beta.6",
+    "rxjs-es": "npm:rxjs-es@5.0.0-beta.6",
+    "zone.js": "npm:zone.js@0.6.12"
+  },
   packages: {}
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
+import 'reflect-metadata';
+import 'zone.js';
 import {myLibA} from './lib';
+import {Component} from '@angular/core';
+// import {Observable} from 'rxjs/Rx';
 
 document.write(myLibA()); 
+// console.log(Observable, 5)
+console.log(Component, 5)
+
+const x = 'foo1';
+const y = 'bar2';
+console.log({x,y})
